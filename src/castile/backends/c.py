864fd0a1d08000280c53b8cbfb95eb624977836a
Chris Pressey <cpressey@catseye.tc>
2021-06-29 15:51:34 +0100

    Slightly prettier code generation in C backend.

--- src/castile/backends/c.py
+++ src/castile/backends/c.py
@@ -7,11 +7,46 @@ OPS = {
   'or': '||',
 }
 
+PRELUDE = r"""
+/* AUTOMATICALLY GENERATED -- EDIT AT OWN RISK */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void print(char *s)
+{
+    printf("%s\n", s);
+}
+
+char *concat(char *s, char *t)
+{
+    char *st = malloc(strlen(s) + strlen(t) + 1);
+    st[0] = '\0';
+    strcat(st, s);
+    strcat(st, t);
+    return st;
+}
+
+struct tagged_value {
+    int type;
+    union {
+        void *ptr;
+        int i;
+    };
+};
+
+struct tagged_value *tag(int type, void *value) {
+}
+
+"""
+
 
 class Compiler(object):
     def __init__(self, out):
         self.out = out
         self.main_type = None
+        self.indent = 0
 
     def commas(self, asts, sep=','):
         if asts:
@@ -19,6 +54,13 @@ class Compiler(object):
                 self.compile(child)
                 self.out.write(sep)
             self.compile(asts[-1])
+
+    def write(self, x):
+        self.out.write(x)
+
+    def write_indent(self, x):
+        self.out.write('  ' * self.indent)
+        self.out.write(x)
 
     # as used in local variable declarations
     def c_type(self, type):
@@ -53,77 +95,39 @@ class Compiler(object):
         else:
             return '%s %s' % (self.c_type(type), name)
 
+    def compile_postlude(self):
+        self.write(r"""
+
+int main(int argc, char **argv)
+{""")
+        if self.main_type == Void():
+            self.write(r"""
+    castile_main();
+""")
+        if self.main_type == Integer():
+            self.write(r"""
+    int x = castile_main();
+    printf("%d\n", x);
+""")
+        if self.main_type == Boolean():
+            self.write(r"""
+    int x = castile_main();
+    printf("%s\n", x ? "True" : "False");
+""")
+        self.write(r"""
+    return 0;
+}
+
+""")
+
     def compile(self, ast):
         if ast.tag == 'Program':
             g = VarDeclTypeAssigner()
             g.assign_types(ast)
-            self.out.write(r"""
-/* AUTOMATICALLY GENERATED -- EDIT AT OWN RISK */
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-void print(char *s)
-{
-  printf("%s\n", s);
-}
-
-char *concat(char *s, char *t)
-{
-  char *st = malloc(strlen(s) + strlen(t) + 1);
-  st[0] = '\0';
-  strcat(st, s);
-  strcat(st, t);
-  return st;
-}
-
-struct tagged_value {
-    int type;
-    union {
-        void *ptr;
-        int i;
-    };
-};
-
-struct tagged_value *tag(int type, void *value) {
-}
-
-""")
+            self.write(PRELUDE)
             for child in ast.children:
                 self.compile(child)
-            if self.main_type == Void():
-                self.out.write(r"""
-
-int main(int argc, char **argv)
-{
-    castile_main();
-    return 0;
-}
-
-""")
-            if self.main_type == Integer():
-                self.out.write(r"""
-
-int main(int argc, char **argv)
-{
-    int x = castile_main();
-    printf("%d\n", x);
-    return 0;
-}
-
-""")
-            if self.main_type == Boolean():
-                self.out.write(r"""
-
-int main(int argc, char **argv)
-{
-    int x = castile_main();
-    printf("%s\n", x ? "True" : "False");
-    return 0;
-}
-
-""")
+            self.compile_postlude()
         elif ast.tag == 'Defn':
             thing = ast.children[0]
             name = ast.value
@@ -131,31 +135,35 @@ int main(int argc, char **argv)
                 name = 'castile_main'
                 self.main_type = thing.type.return_type
             if thing.tag == 'FunLit':
-                self.out.write(self.c_decl(thing.type, name, args=False))
+                self.write(self.c_decl(thing.type, name, args=False))
                 self.compile(ast.children[0])
             else:
-                self.out.write('%s = ' % ast.value)
+                self.write_indent('%s = ' % ast.value)
                 self.compile(ast.children[0])
-                self.out.write(';\n')
+                self.write(';\n')
         elif ast.tag == 'Forward':
-            self.out.write('extern %s;\n' % self.c_decl(ast.children[0].type, ast.value))
+            self.write_indent('extern %s;\n' % self.c_decl(ast.children[0].type, ast.value))
         elif ast.tag == 'StructDefn':
-            self.out.write('struct %s {' % ast.value)
+            self.write_indent('struct %s {' % ast.value)
+            self.indent += 1
             for child in ast.children:
                 self.compile(child)
-            self.out.write('};\n')
+            self.indent -= 1
+            self.write_indent('};\n')
         elif ast.tag == 'FieldDefn':
-            self.out.write('%s;\n' % self.c_decl(ast.children[0].type, ast.value))
+            self.write_indent('%s;\n' % self.c_decl(ast.children[0].type, ast.value))
         elif ast.tag == 'FunLit':
-            self.out.write('(')
+            self.write_indent('(')
             self.compile(ast.children[0])
-            self.out.write(') {\n')
+            self.write(') {\n')
+            self.indent += 1
             self.compile(ast.children[1])
-            self.out.write('}\n')
+            self.indent -= 1
+            self.write_indent('}\n')
         elif ast.tag == 'Args':
             self.commas(ast.children)
         elif ast.tag == 'Arg':
-            self.out.write('%s %s' % (self.c_type(ast.type), ast.value))
+            self.write('%s %s' % (self.c_type(ast.type), ast.value))
         elif ast.tag == 'Body':
             self.compile(ast.children[0])
             self.compile(ast.children[1])
@@ -163,95 +171,106 @@ int main(int argc, char **argv)
             for child in ast.children:
                 self.compile(child)
         elif ast.tag == 'VarDecl':
-            self.out.write('%s %s;\n' % (self.c_type(ast.type), ast.value))
+            self.write_indent('%s %s;\n' % (self.c_type(ast.type), ast.value))
         elif ast.tag == 'Block':
-            self.out.write('{\n')
+            self.write_indent('{\n')
+            self.indent += 1
             for child in ast.children:
                 self.compile(child)
-                self.out.write(';\n')
-            self.out.write('}\n')
+                self.write(';\n')
+            self.indent -= 1
+            self.write_indent('}\n')
         elif ast.tag == 'While':
-            self.out.write('while (')
+            self.write_indent('while (')
             self.compile(ast.children[0])
-            self.out.write(')\n')
+            self.write(')\n')
+            self.indent += 1
             self.compile(ast.children[1])
+            self.indent -= 1
         elif ast.tag == 'Op':
-            self.out.write('(')
+            self.write('(')
             self.compile(ast.children[0])
-            self.out.write(' %s ' % OPS.get(ast.value, ast.value))
+            self.write(' %s ' % OPS.get(ast.value, ast.value))
             self.compile(ast.children[1])
-            self.out.write(')')
+            self.write(')')
         elif ast.tag == 'VarRef':
-            self.out.write(ast.value)
+            self.write(ast.value)
         elif ast.tag == 'FunCall':
-            self.out.write("((")
-            self.out.write(self.c_decl(ast.children[0].type, '(*)'))
-            self.out.write(")")
+            self.write("((")
+            self.write(self.c_decl(ast.children[0].type, '(*)'))
+            self.write(")")
             self.compile(ast.children[0])
-            self.out.write(")")
-            self.out.write('(')
+            self.write(")")
+            self.write('(')
             self.commas(ast.children[1:])
-            self.out.write(')')
+            self.write(')')
         elif ast.tag == 'If':
-            self.out.write('if (')
+            self.write_indent('if (')
             self.compile(ast.children[0])
-            self.out.write(')\n')
+            self.write(')\n')
             if len(ast.children) == 3:  # if-else
+                self.indent += 1
                 self.compile(ast.children[1])
-                self.out.write(' else\n')
+                self.indent -= 1
+                self.write_indent('else\n')
+                self.indent += 1
                 self.compile(ast.children[2])
+                self.indent -= 1
             else:  # just-if
+                self.indent += 1
                 self.compile(ast.children[1])
+                self.indent -= 1
         elif ast.tag == 'Return':
-            self.out.write('return ')
+            self.write_indent('return ')
             self.compile(ast.children[0])
         elif ast.tag == 'Break':
-            self.out.write('break')
+            self.write_indent('break')
         elif ast.tag == 'Not':
-            self.out.write('!(')
+            self.write('!(')
             self.compile(ast.children[0])
-            self.out.write(')')
+            self.write(')')
         elif ast.tag == 'None':
-            self.out.write('')
+            self.write('')
         elif ast.tag == 'BoolLit':
             if ast.value:
-                self.out.write("1")
+                self.write("1")
             else:
-                self.out.write("0")
+                self.write("0")
         elif ast.tag == 'IntLit':
-            self.out.write(str(ast.value))
+            self.write(str(ast.value))
         elif ast.tag == 'StrLit':
-            self.out.write('"%s"' % ast.value)
+            self.write('"%s"' % ast.value)
         elif ast.tag == 'Assignment':
+            self.write_indent('')
             self.compile(ast.children[0])
-            self.out.write(' = ')
+            self.write(' = ')
             self.compile(ast.children[1])
         elif ast.tag == 'Make':
-            self.out.write('&(struct %s){ ' % ast.type.name)
+            self.write('&(struct %s){ ' % ast.type.name)
             self.commas(ast.children[1:])
-            self.out.write(' }')
+            self.write(' }')
         elif ast.tag == 'FieldInit':
             self.commas(ast.children)
         elif ast.tag == 'Index':
             self.compile(ast.children[0])
-            self.out.write('->%s' % ast.value)
+            self.write('->%s' % ast.value)
         elif ast.tag == 'TypeCast':
-            self.out.write('tag("%s",' % str(ast.children[0].type))
+            self.write('tag("%s",' % str(ast.children[0].type))
             self.compile(ast.children[0])
-            self.out.write(')')
+            self.write(')')
         elif ast.tag == 'TypeCase':
-            self.out.write('if (tag(')
+            self.write_indent('if (tag(')
             self.compile(ast.children[0])
-            self.out.write(") == '%s') {" % str(ast.children[1].type))
-            self.out.write('save=')
+            self.write(") == '%s') {" % str(ast.children[1].type))
+            self.write('save=')
             self.compile(ast.children[0])
-            self.out.write(';\n')
+            self.write(';\n')
             self.compile(ast.children[0])
-            self.out.write('=')
+            self.write('=')
             self.compile(ast.children[0])
-            self.out.write('[1]\n')
+            self.write('[1]\n')
             self.compile(ast.children[2])
             self.compile(ast.children[0])
-            self.out.write(' = save;\n')
+            self.write(' = save;\n')
         else:
             raise NotImplementedError(repr(ast))
